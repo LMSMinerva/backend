e2212114e14777a8e2bf43e76cd15c36b72b2873
David Aristizabal <daristizabal@dparadig.com>
2024-11-26 08:20:10 -0500

    FIX: deploy

--- minerva/minerva/settings.py
+++ minerva/minerva/settings.py
@@ -106,7 +106,10 @@ WSGI_APPLICATION = 'minerva.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
    
