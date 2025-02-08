dda4c4483c4dcd5c1cef4ac5a9aad4fd7438a3fc
altamarluis <laltamar@unal.edu.co>
2025-02-08 17:02:23 -0500

    minor fix

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
-from .views import LoginWithGoogle
+from .views.views import LoginWithGoogle, LogoutView
 
 
 urlpatterns = [
     path("login-with-google/", LoginWithGoogle.as_view(), name="login-with-google"),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('token-verify/', TokenVerifyView.as_view(), name='token-verify'),
-
 ]
